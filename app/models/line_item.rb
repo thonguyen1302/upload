3611153ed2aed8aafe5667395ffc35e09ce6544e
thonguyen1302 <nguyentantho1302@gmail.com>
2012-10-08 16:06:24 +0700

    six commit

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,9 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
+  def total_price
+  	product.price * quantity
+  end
 
   attr_accessible :cart_id, :product_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121008081423) do
+ActiveRecord::Schema.define(:version => 20121008084607) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -39,8 +39,9 @@ ActiveRecord::Schema.define(:version => 20121008081423) do
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "posts", :force => true do |t|
